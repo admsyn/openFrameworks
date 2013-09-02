3c06b38ab18b80e9ef29edc0a935503ec665f8c2
Adam Carlucci <carlucci.adam@gmail.com>
2013-09-02 12:55:51 -0400

    add define to make it easy to test for ofSoundObject support

--- libs/openFrameworks/sound/ofSoundObject.h
+++ libs/openFrameworks/sound/ofSoundObject.h
@@ -1,9 +1,11 @@
-
 #pragma once
-
 
 #include "ofConstants.h"
 #include "ofBaseTypes.h"
+
+// this #define is for use by addon writers, to conditionally support sound objects in addons
+// (while allowing backwards compatibility with previous versions of openFrameworks)
+#define OF_SOUND_OBJECT
 
 /**
  * ofSoundObject is a node in your dsp chain. It can have one input, 
@@ -12,24 +14,19 @@
  */
 class ofSoundObject: public ofBaseSoundOutput {
 public:
-	
-
 	ofSoundObject();
 	virtual ~ofSoundObject() {}
-	
-	
 	
 	/// Connects the output of this ofSoundObject to the input of the parameter ofSoundObject
 	ofSoundObject &connectTo(ofSoundObject &soundObject);
     void disconnect();
     void disconnectInput(ofSoundObject * input);	
+	
 	/// This is the method you implement to process the signal from inputs to outputs.
-
 	virtual void process(ofSoundBuffer &input, ofSoundBuffer &output) {
 		// default behaviour is pass-through.
 		input.copyTo(output);
 	}
-	
 	
 	/// this pulls the audio through from earlier links in the chain.
 	/// you can override this to add more interesting functionality
@@ -41,8 +38,8 @@ public:
 	/// returns true if there are no infinite loops.
 	virtual bool checkForInfiniteLoops();
     ofSoundObject *getInputObject();
-protected:
 	
+protected:
 	// this is the previous dsp object in the chain
 	// that feeds this one with input.
 	ofSoundObject *inputObject;
@@ -50,18 +47,13 @@ protected:
     virtual void setInput(ofSoundObject *obj);
 	
 private:
-	
 	// ofSoundObjects reference their source, not their destination
 	// because it's not needed in a pullthrough audio architecture.
 	// this lets that be set under the hood via connectTo()
-	
 
 	// a spare buffer to pass from one sound object to another
-	ofSoundBuffer workingBuffer;
-	
+	ofSoundBuffer workingBuffer;	
 };
-
-
 
 /**
  * This class represents input from the sound card in your dsp chain.
@@ -79,10 +71,7 @@ private:
 
 };
 
-
 /**
  * This class represents the output in your dsp chain.
  */
 class ofSoundOutput: public ofSoundObject {};
-
-
